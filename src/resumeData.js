dccf5bae91686c4af3a93927e69adb8896938c3c
bb3ry <bbambery@gmail.com>
2022-08-10 01:59:30 -0400

    Publish React-Resume

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,63 +1,51 @@
 let resumeData = {
-  "imagebaseurl":"https://techonoeticsclub.github.io/",
-  "name": "John Doe",
-  "role": "Frontend Developer",
-  "linkedinId":"Your LinkedIn Id",
+  "name": "Brendan Bambery",
+  "role": "Frontend/Backend Developer",
+  "linkedinId":"bb3ry",
   "skypeid": "Your skypeid",
-  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "roleDescription": " Hard-working and determined web developer with a flair for creating simple and clean solutions. I have dabbled in both frontend and backend development and enjoy learning about new technologies, to easier solve the problem at hand. In my free time I am into PC games, and Sim-Racing!",
   "socialLinks":[
       {
         "name":"linkedin",
-        "url":"https://www.linkedin.com/",
+        "url":"https://www.linkedin.com/in/bb3ry",
         "className":"fa fa-linkedin"
       },
       {
         "name":"github",
-        "url":"http://github.com/",
+        "url":"https://github.com/bb3ry",
         "className":"fa fa-github"
-      },
-      {
-        "name":"skype",
-        "url":"http://skype.com",
-        "className":"fa fa-skype"
       }
+
     ],
-  "aboutme":"I have finished my B.E. in Computer Science and have been working in industry for past few years.",
-  "address":"India",
-  "website":"https://www.codeclassifiers.com",
+  "aboutme":"I have been at many different roles from sales to construction. I've always had a passion for computers and for the past 7 years have been learning more and more about developing through various projects on my own. From building anf running ecommerce dropshipping businesses using shopify as well as wordpress/woocommerce, to developing and maintaining a fully automated, dynamically generated sports-based web app, with concurrent users up to 50k at times, all while maintaining the same performance as if no users were there.  I have LEMP and LAMP stack experience, managing dedicated servers(Apache/NGINX)(CentOS/Ubuntu), vps, cloud (Digitalocean/OVH) handling everything from building, migrating, database management, auth, security, nginx reverse proxy, and performance... You name it I handled it. Any bugs along the way, its up to me to fix and deploy for many eager fans!",
+  "address":"Boston",
+  "website":"",
   "education":[
     {
-      "UniversityName":"Mumbai University",
-      "specialization":"Some specialization",
+      "UniversityName":"Massasoit",
+      "specialization":"Business",
       "MonthOfPassing":"Jan",
-      "YearOfPassing":"2018",
-      "Achievements":"Some Achievements"
-    },
-    {
-      "UniversityName":"Some University",
-      "specialization":"Some specialization",
-      "MonthOfPassing":"Jan",
-      "YearOfPassing":"2018",
-      "Achievements":"Some Achievements"
+      "YearOfPassing":"2016",
     }
+
   ],
   "work":[
     {
-      "CompanyName":"Some Company",
-      "specialization":"Some specialization",
+      "CompanyName":"Sports Based Web App",
+      "specialization":"Frontend/Backend",
       "MonthOfLeaving":"Jan",
-      "YearOfLeaving":"2018",
-      "Achievements":"Some Achievements"
+      "YearOfLeaving":"2021",
+      "Achievements":"Maintaining performance with concurrent users <= 50k"
     },
     {
-      "CompanyName":"Some Company",
-      "specialization":"Some specialization",
+      "CompanyName":"Ecommerce/Dropshipping",
+      "specialization":"Shopify/Wordpress",
       "MonthOfLeaving":"Jan",
       "YearOfLeaving":"2018",
-      "Achievements":"Some Achievements"
+      "Achievements":"Maintained popular social media presence thru viral video, posts reaching 25k+ users thru Instagram"
     }
   ],
-  "skillsDescription":"Your skills here",
+  "skillsDescription":"My knowledge",
   "skills":[
     {
       "skillname":"HTML5"
@@ -66,39 +54,69 @@ let resumeData = {
       "skillname":"CSS"
     },
     {
+      "skillname":"PHP"
+    },
+    {
+      "skillname":"Javascript"
+    },
+    {
+      "skillname":"Jquery"
+    },
+    {
+      "skillname":"bootstrap"
+    },
+    {
+      "skillname":"Ajax"
+    },
+
+    {
+      "skillname":"mySQL"
+    },
+
+    {
       "skillname":"Reactjs"
-    }
+    },
+    {
+      "skillname":"Vuejs"
+    },
+    {
+      "skillname":"gatsby"
+    },
+    {
+      "skillname":"graphQL"
+    },
+    {
+      "skillname":"Apache"
+    },
+    {
+      "skillname":"Nginx"
+    },
+    {
+      "skillname":"CentOS"
+    },
+    {
+      "skillname":"Ubuntu"
+    },
+    {
+      "skillname":"Wordpress"
+    },
+    {
+      "skillname":"Shopify"
+    },
+
+
   ],
   "portfolio":[
     {
-      "name":"project1",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/phone.jpg"
+      "name":"Sports Finder",
+      "description":"Web App - discontinued",
+      "imgurl":"images/portfolio/sports.png"
     },
-    {
-      "name":"project2",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/project.jpg"
-    },
-    {
-      "name":"project3",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/project2.png"
-    },
-    {
-      "name":"project4",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/phone.jpg"
-    }
   ],
   "testimonials":[
     {
-      "description":"This is a sample testimonial",
-      "name":"Some technical guy"
-    },
-    {
-      "description":"This is a sample testimonial",
-      "name":"Some technical guy"
+      "description":" The design approach is clean and eye-catching. The dark theme followed by white text and flat icons looks great. The user interface is intuitive which makes browsing and navigating around the website quite seamless. Apart from the B&W combination, the grey call-out buttons breathe life into the website.",
+      "name":"Previous Site Review"
     }
   ]
 }
